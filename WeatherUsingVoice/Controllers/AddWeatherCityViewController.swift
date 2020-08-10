a4dddb5f836eb6a9115976ae6763bf53efe25ba2
Saqib Bhatti <saqib.sohail@appsgenii.eu>
2020-08-11 00:25:36 +0500

    SpeechKit framework added and functionality implemented

--- WeatherUsingVoice/Controllers/AddWeatherCityViewController.swift
+++ WeatherUsingVoice/Controllers/AddWeatherCityViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import Speech
 
 protocol AddWeatherDelegate {
     func addWeatherDidSave(vm: WeatherViewModel)
@@ -16,7 +17,48 @@ protocol AddWeatherDelegate {
 class AddWeatherCityViewController: UIViewController {
     
     @IBOutlet weak var cityNamesTextField: UITextField!
+    @IBOutlet weak var microphoneButton: UIButton!
+    @IBOutlet weak var weatherInfoButton: UIButton!
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))
+    
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    
     var delegate: AddWeatherDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        weatherInfoButton.isHidden = true
+        microphoneButton.isEnabled = false
+        speechRecognizer?.delegate = self
+        
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+            var isButtonEnabled = false
+            
+            switch authStatus {
+            case .authorized:
+                isButtonEnabled = true
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            @unknown default:
+                fatalError()
+            }
+            
+            OperationQueue.main.addOperation() {
+                self.microphoneButton.isEnabled = isButtonEnabled
+            }
+        }
+    }
     
     @IBAction func getWeatherInfoBtnTapped(_ sender: UIButton) {
         if let city = cityNamesTextField.text {
@@ -41,5 +83,98 @@ class AddWeatherCityViewController: UIViewController {
     
     @IBAction func closeBtnTapped(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+}
+
+
+
+// Usage of SpeechKit in the extension
+extension AddWeatherCityViewController: SFSpeechRecognizerDelegate {
+    
+    @IBAction func microphoneTapped(_ sender: AnyObject) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            microphoneButton.isEnabled = false
+            microphoneButton.setTitle("Start Recording", for: .normal)
+            weatherInfoButton.isHidden = false
+        }
+        else {
+            startRecording()
+            microphoneButton.setTitle("Stop Recording", for: .normal)
+            weatherInfoButton.isHidden = true
+        }
+    }
+    
+    func startRecording() {
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSession.Category.record) // set the category of the session as recording
+            try audioSession.setMode(AVAudioSession.Mode.measurement)
+            try audioSession.setActive(true)
+        }
+        catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        let inputNode = audioEngine.inputNode
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            
+            var isFinal = false
+            
+            if result != nil {
+                self.cityNamesTextField.text = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {
+                    self.audioEngine.stop()
+                    inputNode.removeTap(onBus: 0)
+                    
+                    self.recognitionRequest = nil
+                    self.recognitionTask = nil
+                    
+                    self.microphoneButton.isEnabled = true
+                }
+            })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) {
+            (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+        }
+        catch {
+            print("audioEngine couldn't start because of an error.")
+        }
+        
+        cityNamesTextField.text = "Say something, I'm listening!"
+    }
+    
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        
+        if available {
+            microphoneButton.isEnabled = true
+        } else {
+            microphoneButton.isEnabled = false
+        }
     }
 }
